96345e65a1601f6ad1a297bea5ed1d8cc91fa21d
Derek Adam <Derekadam1988@gmail.com>
2015-02-04 14:12:30 -0600

    Sorts id attribute

--- Bunny.py
+++ Bunny.py
@@ -7,16 +7,16 @@ class Bunny(object):
 	def __init__(self, id):
 		self.id = id
 
-for i in range(5000000):
+for i in range(10):
 	rng = random.random()	
 	
 	bunnies.append(Bunny(rng))
 
+newlist = sorted(bunnies, key=lambda bunny : bunny.id)
+for bunny in bunnies:
+	print bunny.id
 
-print "done adding"
-print "now sorting"
-newlist = sorted(bunnies, key=lambda bunny: bunny.id)
-'''
+print "*" * 42
+
 for bunny in newlist:
 	print bunny.id
-'''
