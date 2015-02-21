c5c7502f8e9b6878d0f5bf621fd23099a3160918
Derek Adam <Derekadam1988@gmail.com>
2015-02-20 23:30:08 -0600

    Now writes board to file, and reads board.

--- bunnysim.py
+++ bunnysim.py
@@ -1,22 +1,24 @@
 import random
+import time
 
 def loadfile(filename):
     with open(filename, "r") as open_file:
         return [line.strip() for line in open_file]
  
 class Bunny(object):
-	pos = (-1,-1)
+	# pos = (-1,-1)
 	def __init__(self):
 		self.age = 0
 		self.sex = random.choice(("M", "F"))
 		self.name = random.choice(male_names if self.sex == "M" else female_names)
 		self.radioactive = random.randrange(1,100) <= 2
 		self.color = random.choice(colors)
+		self.pos = (-1,-1)
 
 	def is_dead(self):
 		if not self.radioactive:
 			return self.age > 10
-		return self.age > 50
+		return self.age > 10
 
 	def breedable(self):
 		return self.sex == "F" and self.age >= 2 and not self.radioactive
@@ -33,9 +35,25 @@ def make_baby(mother):
 	newbaby.color = mother.color
 	return newbaby
 
+def write_grid(board):
+	with open("boardtest.txt","a") as f:
+
+		for i in range(25):
+			for j in range(25):
+				if not board[i][j] == 0 and not board[i][j].radioactive:
+					f.write(board[i][j].sex + " ")
+				elif not board[i][j] == 0 and board[i][j].radioactive:
+					f.write("X ")
+				else:
+					f.write("0 ")
+			f.write("\n")
+
+		f.write("-" * 72 + "\n")
+
+
 def print_grid(board):
-	for i in range(10):
-		for j in range(10):
+	for i in range(25):
+		for j in range(25):
 			if not board[i][j] == 0 and not board[i][j].radioactive:
 				print board[i][j].sex,
 			elif not board[i][j] == 0 and board[i][j].radioactive:
@@ -45,20 +63,33 @@ def print_grid(board):
 		print ""
 	print "-" * 72
 
+def return_grid(board):
+	result = []
+	for i in range(25):
+		for j in range(25):
+			if not board[i][j] == 0 and not board[i][j].radioactive:
+				result.append(board[i][j].sex)
+			elif not board[i][j] == 0 and board[i][j].radioactive:
+				result.append("X")
+			else:
+				result.append("0")
+
+	return result
+
 #Maybe use filter with function that compares  == 0
 def get_empty_coords(board):
 	emptycoord = list()
-	for i in range(10):
-		for j in range(10):
+	for i in range(25):
+		for j in range(25):
 			if board[i][j] == 0:
 				emptycoord.append((i,j))
 	return emptycoord
 
 def get_normal_bunny_coords(board):
 	emptycoord = list()
-	for i in range(10):
-		for j in range(10):
-			if isinstance(board[i][j], Bunny):
+	for i in range(25):
+		for j in range(25):
+			if isinstance(board[i][j], Bunny) and not board[i][j].radioactive:
 				emptycoord.append((i,j))
 
 	return emptycoord
@@ -105,6 +136,15 @@ def cycle(bunnies, board):
 				board[x][y] = 0
 				continue
 
+			if bunny.pos != (-1,-1) and list(filter(lambda x: not x.radioactive, bunnies)):
+
+				possible_moves = find_neighbors(bunny, get_empty_coords(board))
+
+				if possible_moves:
+					chosen_move = random.choice(possible_moves)
+					move(chosen_move, bunny, board)
+
+
 			if bunny.breedable():
 				male_bunnies = filter(lambda x: x.sex == "M", bunnies)
 				if male_bunnies:
@@ -118,30 +158,39 @@ def cycle(bunnies, board):
 			if bunny.radioactive:
 				normal_bunnies = list(filter(lambda x: not x.radioactive, bunnies))
 				if normal_bunnies:
+
 					coords = find_neighbors(bunny, get_normal_bunny_coords(board))
-					coords = random.choice(coords)
+	
+					if coords:
+						coords = random.choice(coords)
+					
 					for b in normal_bunnies:
-						if b.pos == coords:
+						if b.pos == coords and not b.radioactive:
 							b.radioactive = True
 
-			#print bunny.get_stats()
+			# print bunny.get_stats()
 
 			if bunny.pos == (-1,-1) and len(get_empty_coords(board)) != 0:
 				pos = random.choice(get_empty_coords(board))
 				move(pos, bunny, board)
 
 
+		f.write("\n")
 		for bunny in bunnies:
 			f.write(str(bunny.get_stats()) + "\n")
+		f.write("-" * 72)
 
 def main():
 	#Initialize bunny list
 	bunnies = [Bunny() for i in range(5)]
 	
 	#Initialize a 3 by 3 board filled with 0's
-	board = [[0 for col in range(10)]for row in range(10)]
+	board = [[0 for col in range(25)]for row in range(25)]
 
 	with open("bunnyinfo.txt", "w") as f:
+		f.write("")
+
+	with open("boardtest.txt", "w") as f:
 		f.write("")
 
 	while bunnies:
@@ -150,7 +199,26 @@ def main():
 			random.shuffle(bunnies)
 			bunnies = bunnies[:len(bunnies) // 2]
 
-		print_grid(board)
+		# with open("boardtest.txt", "a") as f:
+		# 	for i in range(25):
+		# 		a = board[i]
+		# 		a = str(a)
+		# 		f.write(a)
+		# 		f.write("\n")
+				
+		write_grid(board)
+		# print_grid(board)
+		# with open("bunnyboard.txt", "w") as f:
+		# 	f.write(print_grid(board))
+
+	with open("boardtest.txt", "r") as f:
+		for line in f:
+			if line.find("-") == 0:
+				time.sleep(.10)
+				print"\n"
+			else:
+				print line,
+
 
 if __name__ == '__main__':
 	male_names = loadfile("names_male.txt")
